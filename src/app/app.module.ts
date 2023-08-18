0c0ca7f62e886e427bb792097fc98d4c5151d3a6
watipaska <mwatipaska@gmail.com>
2023-08-18 18:05:27 +0200

    added Html components working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+import { SingleComponent } from './single/single.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    SingleComponent
   ],
   imports: [
     BrowserModule
